0a70293b939c9a49ebbfeaedac053288d8acca15
KidCharles <sircharlyrivera@gmail.com>
2025-06-20 15:13:23 -0400

    Implemented add-columns feature

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // Add row feature
-const addRowBtn = document.getElementById("add-row");
+const addRowFeature = document.getElementById("add-row");
+const addColFeature = document.getElementById("add-column")
 const tbody = document.querySelector("table tbody");
 
-addRowBtn.onclick = function () {
+addRowFeature.onclick = function () {
   const cols = tbody.rows[0].cells.length;
   const tr = document.createElement("tr");
   for (let i = 0; i < cols; i++) tr.appendChild(document.createElement("td"));
   tbody.appendChild(tr);
 };
 
+
+// Add-columns feature
+addColFeature.onclick = function () {
+  for (let row of tbody.rows) {
+    row.appendChild(document.createElement("td"));
+  }
+};
