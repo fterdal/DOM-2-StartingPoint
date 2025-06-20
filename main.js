4560759e79003c348f1d45b627419eca2c4397ce
KidCharles <sircharlyrivera@gmail.com>
2025-06-20 14:54:56 -0400

    Implemented add-rows feature

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-// Please feel free to change the JS as you see fit! This is just a starting point.
+// Add row feature
+const addRowBtn = document.getElementById("add-row");
+const tbody = document.querySelector("table tbody");
 
-const root = document.getElementById("root");
-root.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-  console.log(event.target);
-});
+addRowBtn.onclick = function () {
+  const cols = tbody.rows[0].cells.length;
+  const tr = document.createElement("tr");
+  for (let i = 0; i < cols; i++) tr.appendChild(document.createElement("td"));
+  tbody.appendChild(tr);
+};
+
