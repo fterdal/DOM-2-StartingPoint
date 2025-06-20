5c0c36beea5882ebd3157043b68300ef379c2f9f
Alex Nurci <alexnurci@Alexs-MacBook-Air.local>
2025-06-20 14:19:28 -0400

    Added color changing on single cell, fill uncolored cells and fill all cells with a selected color

--- main.js
+++ main.js
@@ -4,4 +4,28 @@ const root = document.getElementById("root");
 root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
+  console.log(event.target.style.backgroundColor);
+  const color = document.getElementById("color-select");
+  if(event.target.tagName == "TD"){
+    const selected = color.value;
+    event.target.style.backgroundColor = selected;
+  }
+  const uncoloredButton = document.getElementById("fill-uncolored grid");
+  uncoloredButton.addEventListener("click", () => {
+    allTD = document.querySelectorAll('td');
+    allTD.forEach(td => {
+      if(td.style.backgroundColor == ""){
+        const selected = color.value;
+        td.style.backgroundColor = selected;
+      }
+    });
+  });
+  const fillButton = document.getElementById("fill-grid");
+  fillButton.addEventListener("click", () => {
+    allTD = document.querySelectorAll('td');
+    allTD.forEach(td => {
+      const selected = color.value;
+      td.style.backgroundColor = selected;
+    });
+  });
 });
