0791300dc69613e9a4db3d5ab074926992ceae49
Joseph <sourpina@gmail.com>
2025-06-20 13:59:34 -0400

    Resolved #10
    - Added a function for getting the current color

--- main.js
+++ main.js
@@ -2,30 +2,39 @@
 
 const root = document.getElementById("root");
 const allCells = [...document.querySelectorAll("td")];
+let filling = false;
 root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 
   if (event.target.id === "fill-grid") {
-    const colorSelect = document.getElementById("color-select");
-    const currentColor = colorSelect.value;
-    
     // Fill all uncolored cells with the currently selected color
 
     // Fill all cells with the currently selected color
-    allCells.forEach((e) => {e.style.backgroundColor = currentColor;});
+    allCells.forEach((e) => { e.style.backgroundColor = GetCurrentColor(); });
   } 
   else if (event.target.id === "clear-grid") {
     // Clear all cells/restore all cells to their original/initial color
-    allCells.forEach((e) => {e.style.backgroundColor = "";});
+    allCells.forEach((e) => { e.style.backgroundColor = ""; });
   }
-  else if (event.target.tagName === "TD") {
-    // Click and hold (mouseover) from a single cell (start) to a different cell (end)
-    // such that all affected/hovered-over cells from start to end change to the currently
-    // selected color
-
-  }
+  
+  filling = false;
 });
 
+// Click and hold (mouseover) from a single cell (start) to a different cell (end)
+// such that all affected/hovered-over cells from start to end change to the currently
+// selected color
+allCells.forEach((e) => { 
+    e.addEventListener("mousedown",  (event) => {
+        filling = true;
+        e.style.backgroundColor = GetCurrentColor();
+    }); 
+    e.addEventListener("mouseover", (event) => {
+        if (filling)
+            e.style.backgroundColor = GetCurrentColor();
+    });
+});
 
-
+function GetCurrentColor() {
+    return document.getElementById("color-select").value;
+}
