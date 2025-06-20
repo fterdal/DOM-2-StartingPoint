4bc846d5bf726b4a511f9650908045826f99eee3
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-20 11:01:58 -0700

    Debbie is awesome

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+const button = document.getElementById("add-column");
+
+button.addEventListener("click", () => {
+  const rows = document.querySelectorAll("table tr");
+  rows.forEach(row => {
+    const newCell = document.createElement("td");
+    row.appendChild(newCell);
+  });
+});
+
